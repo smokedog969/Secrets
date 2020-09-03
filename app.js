bf173dd42d392f972f661b6642c203de0eab65fd
smokedog969 <steffen.schafrik@googlemail.com>
2020-09-03 11:29:04 +0200

    ADD Environment Vars and MD5 Encrytion

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+//const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app=express();
 
-console.log(process.env.API_KEY)
- 
+//console.log(process.env.API_KEY)
+console.log(md5("123456"));
+
 app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
@@ -21,8 +23,8 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// PLUGIN for MOGOOSE ENCRYPTION
+//userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -39,7 +41,7 @@ app.get("/register",function(req,res){
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function (err){
@@ -53,7 +55,7 @@ app.post("/register", function(req, res){
  
 app.post("/login", function(req, res){ 
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function (err, foundUser){
         if (err) {
